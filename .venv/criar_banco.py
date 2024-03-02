59bed617ba72cd181359001965fe2b7649161a2f
mcndevlab <mcnascimento@gmail.com>
2024-03-02 17:28:47 -0300

    Adicionar .venv ao .gitignore

--- .venv/criar_banco.py
+++
@@ -1,8 +0,0 @@
-# arquivo temporario existe apenas para criar o banco, após criado perde a função...
-from fakepinterest import database, app  # aula 43.9
-from fakepinterest.models import Usuario, Foto
-# para criar o banco de dados..
-with app.app_context():  # aula 43.9
-    database.create_all()  # aula 43.9
-# Ao executar os comandos acima é criado a pasta "instance" que é uma instancia onde está nosso
-# Banco de dados comunidade.db - BD SQL LIGHT (conforme definimos no init..)
--- .venv/main.py
+++
@@ -1,7 +0,0 @@
-import sys
-sys.path.append(".venv")
-
-from fakepinterest import app
-
-if __name__== "__main__":
-    app.run(debug=True)
